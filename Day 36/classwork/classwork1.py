597d72748cd04aa7958026772847958a53186757
IotsertsvaIo <tsertsvadzeio7@gmail.com>
2024-06-03 14:30:38 +0400

    commit

---
+++ Day 36/classwork/classwork1.py
@@ -0,0 +1,12 @@
+About_myself = {
+
+    "name" : "ioane",
+    "surname" : "tsertsvadze",
+    "age" : "16"
+
+} 
+
+to_join = About_myself.values
+values_join = " ".join([str i for i in to_join]) 
+
+print(values_join)
---
+++ Day 36/homework/homework1.py
@@ -0,0 +1,12 @@
+person = {
+    "name" : "ioane",
+    "surname" : "tsertsvadze", 
+    "age" : "12",
+    "country_from" : "geaorgia"
+}
+
+x = person.values
+
+values_join = " ".join([for i in x])
+print(values_join)
+
